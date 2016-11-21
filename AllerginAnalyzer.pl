9f361e4c38869a157c659c4faf2927fa07f5ebbb
Bill Chatfield <bill_chatfield@yahoo.com>
2016-11-21 18:50:03 -0500

    Completed with graph generation

--- AllerginAnalyzer.pl
+++ AllerginAnalyzer.pl
@@ -1,9 +1,9 @@
 use Mojo::UserAgent;
 use Chart::Gnuplot;
 use strict;
-use Data::Dumper;
 
 sub loadTable($);
+sub zeroOutYears($);
 
 # Fix insecure PATH bs when starting gnuplot.
 $ENV{PATH} = '/bin:/usr/bin';
@@ -15,60 +15,103 @@ my @pages = (
 	'http://www.rapca.org/datafiles/PollenMoldSum16.htm'
 );
 
-my $chart = new Chart::Gnuplot(
-	output => "allergin-graph.ps",
-	title  => "Comparison of Allergins by Year",
-	xlabel => "Pollen & Mold Spore Count",
-	ylabel => "Date",
-    timeaxis  => "x"
+my $alternariaChart = new Chart::Gnuplot(
+	output => "alternaria-comparison-by-year.ps",
+	title  => "Alternaria Comparison by Year",
+	xlabel => "Date",
+	ylabel => "Count",
+    timeaxis  => "x",
+    xtics => { labelfmt => "%b %d", rotate => -90},
+    xrange => ['4/1/00', '12/1/00']
+);
+
+my $ambrosiaChart = new Chart::Gnuplot(
+	output => "ambrosia-comparison-by-year.ps",
+	title  => "Ambrosia Comparison by Year",
+	xlabel => "Date",
+	ylabel => "Count",
+    timeaxis  => "x",
+    xtics => { labelfmt => "%b %d", rotate => -90},
+    xrange => ['7/1/00', '12/1/00']
 );
 
 my $userAgent = new Mojo::UserAgent;
-my @dataSets = ();
+my @alternariaDataSets;
+my @ambrosiaDataSets;
 
 foreach my $page (@pages) {
+    print "Reading $page\n";
 	my $dom = $userAgent->get($page)->res->dom;
     my $pollenTableRef = loadTable($dom->find('table tr:nth-of-type(3)')->first);
     my $moldTableRef = loadTable($dom->find('table:nth-of-type(2) tr:nth-of-type(2)')->first);
+    my ($year) = $moldTableRef->{Date}[0] =~ /(\d\d\d\d)$/;
 
-	my $dataSet = new Chart::Gnuplot::DataSet(
+    zeroOutYears($moldTableRef->{"Date"});
+    zeroOutYears($pollenTableRef->{"Date"});
+    
+	my $alternariaDataSet = new Chart::Gnuplot::DataSet(
 		xdata => $moldTableRef->{"Date"},
 		ydata => $moldTableRef->{"Alternaria"},
-		title => "2013",
+		title => $year,
 		style => "linespoints",
         timefmt => "%m/%d/%Y"
 	);
-
-    push(@dataSets, $dataSet);
+    
+	my $ambrosiaDataSet = new Chart::Gnuplot::DataSet(
+		xdata => $pollenTableRef->{"Date"},
+		ydata => $pollenTableRef->{"Ambrosia"},
+		title => $year,
+		style => "linespoints",
+        timefmt => "%m/%d/%Y"
+	);
+    
+    push(@alternariaDataSets, $alternariaDataSet);
+    push(@ambrosiaDataSets, $ambrosiaDataSet);
 }
 
-$chart->plot2d($dataSets[3]);
+$alternariaChart->plot2d(@alternariaDataSets);
+$ambrosiaChart->plot2d(@ambrosiaDataSets);
 
 sub loadTable($)
 {
 	my ($titleRow) = @_;
     
-	my %pollenTable  = ();
-	my @titles       = ();
+	my %allerginTable; # Allergin Name -> List of counts
+	my @allerginNames;
     
-    # Get titles
+    # Get allerginNames
 	my @titleElements = $titleRow->children->each;
     foreach my $titleElement (@titleElements) {
-        my $title = $titleElement->find('strong')->first->text;
-    	push(@titles, $title);
+        my $allerginName = $titleElement->find('strong')->first->text;
+    	push(@allerginNames, $allerginName);
     }
     
-	my @dailySampleSets = $titleRow->following('tr')->each;
-    foreach my $dailySampleSet (@dailySampleSets) {
-        my @dailyCounts = $dailySampleSet->children->map('text')->each;
-    	for (my $i = 0; $i < @dailyCounts; $i++) {
-            if (!$pollenTable{$titles[$i]}) {
-                $pollenTable{$titles[$i]} = ();
-            }
-            my $arrayRef = $pollenTable{$titles[$i]};
-            push(@$arrayRef, $dailyCounts[$i]);
-            $pollenTable{$titles[$i]} = $arrayRef;
+    # Initialize lists of counts by allergin name.
+    foreach my $allerginName (@allerginNames) {
+        # Create an empty array and assign its reference to this allergin name.
+        $allerginTable{$allerginName} = [];
+    }
+    
+    # Build %allerginTable from data rows.
+	my @dataRows = $titleRow->following('tr')->each;
+    foreach my $dataRow (@dataRows) {
+        my @allCountsForOneDay = $dataRow->children->map('text')->each;
+    	for (my $allerginIndex = 0; $allerginIndex < @allCountsForOneDay; $allerginIndex++) {
+            my $allerginName = $allerginNames[$allerginIndex];
+            my $countsForAllerginRef = $allerginTable{$allerginName};
+            # Add this count to the list for this allergin.
+            push(@$countsForAllerginRef, $allCountsForOneDay[$allerginIndex]);
+            # Put the reference to the list back in the table. This seems to be necessary.
+            $allerginTable{$allerginName} = $countsForAllerginRef;
     	}
     }
-    return \%pollenTable;
+    return \%allerginTable;
+}
+
+sub zeroOutYears($)
+{
+    my ($dateArrayRef) = @_;
+    for (my $i = 0; $i < @$dateArrayRef; $i++) {
+        $dateArrayRef->[$i] =~ s/\d\d\d\d$/00/;
+    }
 }
